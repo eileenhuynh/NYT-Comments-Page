84e53417e08beec7e8f7c26d34f0e1899228d7af
eileenhuynh <eileenhuynh1064@gmail.com>
2025-05-14 15:05:00 -0700

    merged hw 2

--- hw3-app/backend/app.py
+++ hw3-app/backend/app.py
@@ -1,34 +1,112 @@
 from flask import Flask, jsonify, send_from_directory
 import os
+import requests
 from flask_cors import CORS
-from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
-static_path = os.getenv('STATIC_PATH','static')
-template_path = os.getenv('TEMPLATE_PATH','templates')
-# Mongo connection
-mongo_uri = os.getenv("MONGO_URI")
-mongo = MongoClient(mongo_uri)
-db = mongo.get_default_database()
+load_dotenv() 
 
+app = Flask(__name__, static_folder="static", template_folder="templates")
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173"]}})
 
-app = Flask(__name__, static_folder=static_path, template_folder=template_path)
-CORS(app)
-
-@app.route('/api/key')
+@app.route("/api/key")
 def get_key():
-    return jsonify({'apiKey': os.getenv('NYT_API_KEY')})
+    return jsonify({"apiKey": os.getenv("NYT_API_KEY")})
 
-@app.route('/')
-@app.route('/<path:path>')
-def serve_frontend(path=''):
-    if path != '' and os.path.exists(os.path.join(static_path,path)):
-        return send_from_directory(static_path, path)
-    return send_from_directory(template_path, 'index.html')
+@app.route("/api/local-news")
+def get_local_news():
+    api_key = os.getenv("NYT_API_KEY")
+    if not api_key:
+        return jsonify({"error": "API key not found"}), 500
 
-@app.route("/test-mongo")
-def test_mongo():
-    return jsonify({"collections": db.list_collection_names()})
+    end_date = datetime.now().strftime('%Y%m%d')
+    begin_date = (datetime.now() - timedelta(days=365)).strftime('%Y%m%d')  # Date up to 1 yr ago
 
-if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)),debug=debug_mode)
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    params = {
+        "q": "Sacramento (Calif) OR Davis (Calif)",
+        "api-key": api_key,
+        "begin_date": begin_date,
+        "end_date": end_date,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/")
+@app.route("/<path:path>")
+def serve_frontend(path=""):
+    if path != "" and os.path.exists(f"static/{path}"):
+        return send_from_directory("static", path)
+    return send_from_directory("templates", "index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
+
+# from flask import Flask, jsonify, send_from_directory
+# import os
+# import requests
+# from flask_cors import CORS
+# from dotenv import load_dotenv
+# from datetime import datetime, timedelta
+# from pymongo import MongoClient
+
+# static_path = os.getenv('STATIC_PATH','static')
+# template_path = os.getenv('TEMPLATE_PATH','templates')
+# # Mongo connection
+# mongo_uri = os.getenv("MONGO_URI")
+# mongo = MongoClient(mongo_uri)
+# db = mongo.get_default_database()
+
+
+# app = Flask(__name__, static_folder=static_path, template_folder=template_path)
+# CORS(app)
+
+# @app.route('/api/key')
+# def get_key():
+#     return jsonify({'apiKey': os.getenv('NYT_API_KEY')})
+
+# @app.route("/api/local-news")
+# def get_local_news():
+#     api_key = os.getenv("NYT_API_KEY")
+#     if not api_key:
+#         return jsonify({"error": "API key not found"}), 500
+
+#     end_date = datetime.now().strftime('%Y%m%d')
+#     begin_date = (datetime.now() - timedelta(days=365)).strftime('%Y%m%d')  # Date up to 1 yr ago
+
+#     url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+#     params = {
+#         "q": "Sacramento (Calif) OR Davis (Calif)",
+#         "api-key": api_key,
+#         "begin_date": begin_date,
+#         "end_date": end_date,
+#     }
+
+#     try:
+#         response = requests.get(url, params=params)
+#         response.raise_for_status()
+#         return jsonify(response.json())
+#     except requests.RequestException as e:
+#         return jsonify({"error": str(e)}), 500
+    
+# @app.route('/')
+# @app.route('/<path:path>')
+# def serve_frontend(path=''):
+#     if path != '' and os.path.exists(os.path.join(static_path,path)):
+#         return send_from_directory(static_path, path)
+#     return send_from_directory(template_path, 'index.html')
+
+# @app.route("/test-mongo")
+# def test_mongo():
+#     return jsonify({"collections": db.list_collection_names()})
+
+# if __name__ == '__main__':
+#     debug_mode = os.getenv('FLASK_ENV') != 'production'
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)),debug=debug_mode)
--- hw3-app/frontend/src/App.svelte
+++ hw3-app/frontend/src/App.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
+  // import svelteLogo from './assets/svelte.svg';
+  // import viteLogo from '/vite.svg';
+  // import Counter from './lib/Counter.svelte';
 
   let apiKey: string = '';
+  let today = '';
+  let articles: any[] = [];
+  let imageArticles: any[] = [];
+
+  function getImage(article: any): string | null {
+    if (article?.multimedia?.default?.url) {
+      return article.multimedia.default.url;
+    } else if (article?.multimedia?.thumbnail?.url) {
+      return article.multimedia.thumbnail.url;
+    }
+    return null;
+  }
 
   onMount(async () => {
+    const now = new Date();
+    today = now.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+
     try {
-      const res = await fetch('/api/key');
+      const res = await fetch('http://localhost:8000/api/local-news');
       const data = await res.json();
-      apiKey = data.apiKey;
+      articles = data.response?.docs || [];
+      imageArticles = articles.filter(article => getImage(article));
     } catch (error) {
       console.error('Failed to fetch API key:', error);
     }
@@ -18,34 +39,97 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <header>
+    <div class="date">
+      <p><b>{today}</b><br/> Today’s Paper</p>
+      <br />
+    </div>
+    <br>
+    <div class="logo-image">
+      <img src="/NYTimeslogo.png" alt="The New York Times" />
+    </div>
+    <hr style="margin: 1% 2% 2% 2%; width: 95%" />
+  </header>
 
-  <div class="card">
+  {#if imageArticles.length > 0}
+    <div class="main-container">
+      <div class="left-column">
+        <img src={getImage(imageArticles[2])} alt={imageArticles[2].headline.main}/>
+        <h2>{imageArticles[2].headline.main}</h2>
+        <p>{imageArticles[2].abstract}</p>
+        <p>{imageArticles[2].lead_paragraph}</p>
+        <hr style="margin: 2% 0% 10% 0%"/>
+
+        <img src={getImage(imageArticles[3])} alt={imageArticles[3].headline.main}/>
+        <h2>{imageArticles[3].headline.main}</h2>
+        <p>{imageArticles[3].abstract}</p>
+        <p>{imageArticles[3].lead_paragraph}</p>
+      </div>
+
+      <div class="mid-column">
+        <img src={getImage(imageArticles[0])} alt={imageArticles[0].headline.main}/>
+        <h1>{imageArticles[0].headline.main}</h1>
+        <p>{imageArticles[0].abstract}</p>
+        <p>{imageArticles[0].lead_paragraph}</p>
+        <hr style="margin: 1% 0% 5% 0%"/>
+
+        <img src={getImage(imageArticles[1])} alt={imageArticles[1].headline.main}/>
+        <h3>{imageArticles[1].headline.main}</h3>
+        <p>{imageArticles[1].abstract}</p>
+        <p>{imageArticles[1].lead_paragraph}</p>
+      </div>
+
+      <div class="right-column">
+        <img src={getImage(imageArticles[5])} alt={imageArticles[5].headline.main}/>
+        <h2>{imageArticles[5].headline.main}</h2>
+        <p>{imageArticles[5].abstract}</p>
+        <p>{imageArticles[5].lead_paragraph}</p>
+        <hr style="margin: 2% 0% 10% 0%"/>
+
+        <img src={getImage(imageArticles[7])} alt={imageArticles[7].headline.main}/>
+        <h2>{imageArticles[7].headline.main}</h2>
+        <p>{imageArticles[7].abstract}</p>
+        <p>{imageArticles[7].lead_paragraph}</p>
+      
+      </div>
+
+      <div class="side-columns">
+        <img src={getImage(imageArticles[2])} alt={imageArticles[2].headline.main}/>
+        <h2>{imageArticles[2].headline.main}</h2>
+        <p>{imageArticles[2].abstract}</p>
+        <p>{imageArticles[2].lead_paragraph}</p>
+        <hr style="margin: 5% 0% 10% 0%"/>
+
+        <img src={getImage(imageArticles[3])} alt={imageArticles[3].headline.main}/>
+        <h2>{imageArticles[3].headline.main}</h2>
+        <p>{imageArticles[3].abstract}</p>
+        <p>{imageArticles[3].lead_paragraph}</p>
+        <hr style="margin: 5% 0% 10% 0%"/>
+
+        <img src={getImage(imageArticles[5])} alt={imageArticles[5].headline.main}/>
+        <h2>{imageArticles[5].headline.main}</h2>
+        <p>{imageArticles[5].abstract}</p>
+        <p>{imageArticles[5].lead_paragraph}</p>
+        <hr style="margin: 5% 0% 10% 0%"/>
+
+        <img src={getImage(imageArticles[7])} alt={imageArticles[7].headline.main}/>
+        <h2>{imageArticles[7].headline.main}</h2>
+        <p>{imageArticles[7].abstract}</p>
+        <p>{imageArticles[7].lead_paragraph}</p>
+      </div>
+    </div>
+  {:else}
+    <p>Loading articles...</p>
+  {/if}
+  <hr style="height: 3px; background-color: black; margin: 2% 2% 3% 2%" />
+
+  <!-- <div class="card">
     <Counter />
-  </div>
+  </div> -->
 
-  <p>
-    Your API Key: <strong>{apiKey}</strong>
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
-<style>
+<!-- <style>
   .logo {
     height: 6em;
     padding: 1.5em;
@@ -61,4 +145,4 @@
   .read-the-docs {
     color: #888;
   }
-</style>
+</style> -->
